96b9028fb10ba5f3ae4731700eb35177f08b57f0
risato320higashi <ritsu320gg@gmail.com>
2025-07-15 11:39:13 +0900

    ランダム生成を1以上99までに変更

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,4 +1,4 @@
-let num = Math.floor(Math.random() * 100); //0から99までのランダムな整数を生成
+let num = Math.floor(Math.random() * 100)+1; //1から99までのランダムな整数を生成
 
 if (num % 3 === 0 && num % 5 === 0) { //numが3と5の両方で割り切れるかチェック
     console.log("3と5の倍数です");
