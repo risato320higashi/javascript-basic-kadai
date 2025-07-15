68d2d5cbb1097014b708b2be940e2e5dbc05eeab
risato320higashi <ritsu320gg@gmail.com>
2025-07-15 20:02:10 +0900

    mdn.jsを修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,14 +1,11 @@
 // 現在の日付を取得
-const today = Date();
-
-// Date()は現在の日時を表す文字列を返す
-const toStringToday = new Date(today);
+const today = new Date();
 
 //年月日を配列に格納 
 const nowDate = [
-  toStringToday.getFullYear(),
-  toStringToday.getMonth()+1, // getMonth() returns 0-11, so we add 1
-  toStringToday.getDate()
+  today.getFullYear(),
+  today.getMonth()+1, // getMonth() returns 0-11, so we add 1
+  today.getDate()
 ]
 
 // 年月日を表示
